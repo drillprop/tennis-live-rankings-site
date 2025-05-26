87e2d15a302ac273b6c545b6352d0a1312570d9b
drillprop <drylbartosz@gmail.com>
2025-05-26 23:12:06 +0200

    introduce prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,39 +4,37 @@ import "./globals.css";
 import { Header } from "@/components/header/header";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: {
-    template: "Live Tennis Rankings | %s",
-    default: "Live Tennis Rankings",
-  },
-  description: "Live Tennis Rankings",
-  alternates: {
-    canonical: "/wta-live-ranking",
-  },
+	title: {
+		template: "Live Tennis Rankings | %s",
+		default: "Live Tennis Rankings",
+	},
+	description: "Live Tennis Rankings",
+	alternates: {
+		canonical: "/wta-live-ranking",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<Header />
+				{children}
+			</body>
+		</html>
+	);
 }
--- src/app/wta-live-ranking/page.tsx
+++ src/app/wta-live-ranking/page.tsx
@@ -5,29 +5,29 @@ import { Metadata } from "next";
 import { Suspense } from "react";
 
 async function fetchWtaLiveRanking() {
-  const res = await fetch(`${env.API_URL}/live-ranking/wta`, {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  const validatedData = wtaRankingSchema.parse(data);
-  return validatedData;
+	const res = await fetch(`${env.API_URL}/live-ranking/wta`, {
+		cache: "no-store",
+	});
+	const data = await res.json();
+	const validatedData = wtaRankingSchema.parse(data);
+	return validatedData;
 }
 
 export const metadata: Metadata = {
-  title: "WTA Live Rankings",
-  description: "WTA Live Rankings",
+	title: "WTA Live Rankings",
+	description: "WTA Live Rankings",
 };
 
 function WtaLiveRanking() {
-  const wtaLiveRankingPromise = fetchWtaLiveRanking();
+	const wtaLiveRankingPromise = fetchWtaLiveRanking();
 
-  return (
-    <main className="max-w-screen-lg mx-auto mt-10">
-      <Suspense fallback={<div>Loading...</div>}>
-        <WtaLiveRankingTable wtaLiveRankingPromise={wtaLiveRankingPromise} />
-      </Suspense>
-    </main>
-  );
+	return (
+		<main className="mx-auto mt-10 max-w-screen-lg">
+			<Suspense fallback={<div>Loading...</div>}>
+				<WtaLiveRankingTable wtaLiveRankingPromise={wtaLiveRankingPromise} />
+			</Suspense>
+		</main>
+	);
 }
 
 export default WtaLiveRanking;
